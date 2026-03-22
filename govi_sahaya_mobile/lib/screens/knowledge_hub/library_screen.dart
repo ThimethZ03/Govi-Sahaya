9f10fe3d76cfc3161e1e00dade692123b3dadf6e
W.W.Damsath Nadun Perera <damsathperera1017@gmail.com>
2026-03-22 22:13:36 +0530

    add CategoryChips and dynamic guide list to LibraryScreen

--- govi_sahaya_mobile/lib/screens/knowledge_hub/library_screen.dart
+++ govi_sahaya_mobile/lib/screens/knowledge_hub/library_screen.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
 import '../../config/theme.dart';
 import '../../config/constants.dart';
+import '../../widgets/category_chips.dart';
+import '../../widgets/guide_card.dart';
+import '../../models/guide_model.dart';
+import '../../services/knowledge_hub_service.dart';
 
 class LibraryScreen extends StatefulWidget {
   const LibraryScreen({super.key});
@@ -11,6 +15,31 @@ class LibraryScreen extends StatefulWidget {
 
 class _LibraryScreenState extends State<LibraryScreen> {
   String _selectedCategory = 'All';
+  final KnowledgeHubService _service = KnowledgeHubService();
+  List<GuideModel> _guides = [];
+  bool _isLoading = true;
+
+  final List<String> _categories = [
+    'All',
+    ...AppConstants.libraryCategories,
+  ];
+
+  @override
+  void initState() {
+    super.initState();
+    _loadGuides();
+  }
+
+  Future<void> _loadGuides() async {
+    setState(() => _isLoading = true);
+    final guides = await _service.getGuides(
+      category: _selectedCategory == 'All' ? null : _selectedCategory,
+    );
+    setState(() {
+      _guides = guides;
+      _isLoading = false;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -19,10 +48,7 @@ class _LibraryScreenState extends State<LibraryScreen> {
       appBar: AppBar(
         title: const Text('Knowledge Hub'),
         actions: [
-          IconButton(
-            icon: const Icon(Icons.search),
-            onPressed: () {},
-          ),
+          IconButton(icon: const Icon(Icons.search), onPressed: () {}),
         ],
       ),
       body: Container(
@@ -33,7 +59,34 @@ class _LibraryScreenState extends State<LibraryScreen> {
             topRight: Radius.circular(30),
           ),
         ),
-        child: const Center(child: Text('Loading guides...')),
+        child: Column(
+          children: [
+            const SizedBox(height: 16),
+            CategoryChips(
+              categories: _categories,
+              selectedCategory: _selectedCategory,
+              onCategorySelected: (category) {
+                setState(() => _selectedCategory = category);
+                _loadGuides();
+              },
+            ),
+            const SizedBox(height: 16),
+            Expanded(
+              child: _isLoading
+                  ? const Center(child: CircularProgressIndicator())
+                  : ListView.builder(
+                      padding: const EdgeInsets.all(16),
+                      itemCount: _guides.length,
+                      itemBuilder: (context, index) {
+                        return GuideCard(
+                          guide: _guides[index],
+                          onTap: () {},
+                        );
+                      },
+                    ),
+            ),
+          ],
+        ),
       ),
     );
   }
